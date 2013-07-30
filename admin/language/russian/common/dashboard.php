34914eb082264fd0bb89bbb05fad16ea9419060b
Vadim A. Misbakh-Soloviov <mva@mva.name>
2013-07-30 05:33:06 +0400

    updated russian translation for dashboard
    Signed-off-by: Vadim A. Misbakh-Soloviov <mva@mva.name>

---
+++ admin/language/russian/common/dashboard.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']  = 'Панель управления';
+
+// Text
+$_['text_customers'] = 'Всего покупателей';
+$_['text_orders']    = 'Всего заказов';
+$_['text_clicks']    = 'Переходы';
+$_['text_customers'] = 'Клиенты';
+$_['text_activity']  = 'недавняя активность';
+$_['text_marketing'] = 'Маркетинг';
+$_['text_online']    = 'Люди онлайн';
+$_['text_day']       = 'День';
+$_['text_week']      = 'Неделя';
+$_['text_month']     = 'Месяц';
+$_['text_year']      = 'Год';
+
+// Error
+$_['error_install']  = 'Установочная папка до сих пор существует!';
+?>
